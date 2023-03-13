cee674c46f599ec0c282c7fc29ff0fbaf6da1899
u1trafast <127157320+u1trafast@users.noreply.github.com>
2023-03-13 01:05:02 +0000

    My name is ...

--- pages/index.js
+++ pages/index.js
@@ -17,8 +17,7 @@ export default function Home() {
       <main className={styles.main}>
         <div className={styles.description}>
           <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>pages/index.js</code>
+            My name is u1trafast!
           </p>
           <div>
             <a
